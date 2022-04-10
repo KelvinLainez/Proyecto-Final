3f431fdb21fad40ac717db20cc4b7f12d659b2f7
KelvinLainez <101296285+KelvinLainez@users.noreply.github.com>
2022-04-09 23:40:44 -0600

    Add files via upload

---
+++ ProyectoFinalParcial3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinalparcial3;
+
+/**
+ *
+ * @author pc
+ */
+public class ProyectoFinalParcial3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frmLogin().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
